4768a2c15d086e6845c4f46265f0dda2e7537ad8
Hyeonyeong Kim <hyeonyeongkim@kimpro.local>
2021-07-27 13:53:17 +0900

    file upload- thumnail photo upload and delete

---
+++ src/components/Nweets.js
@@ -0,0 +1,65 @@
+import { dbService } from "myFirebase";
+import React, {useState} from "react";
+
+const Nweet = ({nweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false); // edit 모드인지 아닌지 (true or false) => 수정한다고 하면 edit 모드로 바꿔야하니까.
+    const [newNweet, setNewNweet] = useState(nweetObj.text); // 헷갈리니까 이 부분을 나중에는 [inputText, setInputText]로 바꿔도 될 듯.
+
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete thie nweet?");
+        console.log(ok); // for now, it returns boolean.
+
+        if(ok){
+            await dbService.doc(`nweets/${nweetObj.id}`).delete();
+        }
+    };
+    const toggleEditing = () => setEditing(prev => !prev);
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.doc(`nweets/${nweetObj.id}`).update({
+            text: newNweet
+        });
+        setEditing(false); // editing모드에서 나가도록 false로 셋팅
+    };
+    const onChange = e => {
+        const {
+            target: {value}
+        } = e;
+        setNewNweet(value);
+    };
+    return (
+        <div>
+            {editing? (
+                <>
+                    <form onSubmit = {onSubmit}>
+                        <input
+                        type = "text"
+                        placeholder = "Edit your nweet"
+                        value = {newNweet}
+                        required
+                        onChange = {onChange}
+                        />
+                        <input type = "submit" value = "update Nweet" />
+                    </form>
+                    <button onClick = {toggleEditing}>Cancel</button>
+                </>
+            ):(
+                <>
+                     <h4> {nweetObj.text}</h4>
+                    {/* 본인이 작성한 트윗에만 버튼이 보이도록 */}
+                    {isOwner && (
+                    <>
+                        <button onClick = {onDeleteClick}>delete Nweet</button>
+                        <button onClick = {toggleEditing}>Edit Nweet</button>
+                    </>
+                    )}
+                </>
+            )}
+        </div>
+
+    )
+}
+
+
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweets";
 import { dbService } from "myFirebase";
 import React, {useEffect, useState} from "react";
 
@@ -5,17 +6,22 @@ const Home = ({userObj}) => {
     //console.log(userObj)
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     //mount될 때 트윗 목록 가져오기
     useEffect( () => {
-        dbService.collection("nweets").onSnapshot(snapshot => {
+        dbService.collection("nweets").onSnapshot(snapshot => { //onSnapShot: realtime update
+            //console.log("something happened!")      //뭔가 변화가 생기면 무조건 something happened라는 메세지를 띄우도록.
+            console.log(snapshot.docs) // returns tweet list (means array)
+            //nweetArr는 우리한테 필요한 데이터만 거르는 용도로 사용했음. onSubmit 함수에서 정의한 text, createdAt, creatorId를 doc.data()로 가져오고, id의 값으로 doc.id를 만들어줌
             const nweetArr = snapshot.docs.map(doc => ({    //forEach로 할 수도 있지만 re-rendering 하지 않기 때문에 조금 더 빠르다.
                 id: doc.id,
                 ...doc.data()
             }))
+            console.log(nweetArr)
             setNweets(nweetArr)
         })
-    }, [])
+    }, []) 
     //Whenever we submit, we create a document.
     const onSubmit = async(e) => {
         e.preventDefault();
@@ -34,7 +40,24 @@ const Home = ({userObj}) => {
         } = e;
         setNweet(value);
     }; 
-    return (
+    // 이미지 썸네일 미리보기
+    const onFileChange =(e) => {    //onChange 일 때 이 함수를 실행하는데 함수의 target의 타입이 file임
+        console.log(e.target) // e.target까지만 하면 <input type = "file" accept = "image/*"> 이렇게 나오게 됨                      
+        const {target : {files}} = e;
+        const imgFile = files[0]; // 첫번째 파일만 받도록
+        console.log(imgFile)
+        const reader = new FileReader(); // we create a reader
+        reader.onloadend = (finishedEvent) => { //add an event listener to the reader.  
+            console.log(finishedEvent)
+            const {
+                currentTarget : { result } 
+            } = finishedEvent;
+            setAttachment(result)
+        }
+        reader.readAsDataURL(imgFile);
+    }
+    const onClearAttachmentClick = () => setAttachment(null)
+        return (
         <div>
             <form onSubmit = {onSubmit}>
                 <input 
@@ -43,13 +66,22 @@ const Home = ({userObj}) => {
                 type = "text" 
                 placeholder="What's on your mind?" 
                 maxLength={120} />
+                <input type = "file" accept = "image/*"  onChange = {onFileChange}/> {/* image/* 이라고 하면 이미지만 선택가능하게 함. 그리고 타입이 file인 경우 버튼 이름 안 적어도 자동으로 생성*/}
                 <input type = "submit" value = "Nweet" />
+                {attachment && (
+                <div>
+                    <img src={attachment} width = "50px" height = "50px" />  {/* // attachment가 있다면!! */}
+                    <button onClick = {onClearAttachmentClick}> Clear</button>  {/* 이미지 파일 삭제하기 */}
+                </div>
+                )}
             </form>
             <div>
                 {nweets.map(nweet => (
-                    <div key = {nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet 
+                    key = {nweet.id} 
+                    nweetObj={nweet} 
+                    isOwner = {nweet.creatorId === userObj.uid}
+                    />
                 ))}
             </div>
         </div>
